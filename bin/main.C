09d2d89f2afe5297d8a929d25d952e25f02c1395
Kirill Shakirov <kirill.shakirov4@gmail.com>
2026-07-26 20:09:46 +0300

    filter done

--- bin/main.C
+++ bin/main.C
@@ -1,13 +1,9 @@
+using namespace std;
 #include <iostream>
-#include "all.hpp"
 #include "../lib/rel_operator/rel_operator.hpp"
 
 
 
-//import std;
-//import lattice_storage;
-//import rel_operator;
-//#include <iostream>
 
 int main(int argc, char** argv){
   cout<< "Emulating our rel engine"<< endl;
--- lib/rel_operator/rel_operator.C
+++ lib/rel_operator/rel_operator.C
@@ -1,4 +1,4 @@
-#include "../../bin/all.hpp"
+#include "../all.hpp"
 
 void Filter(int x){
   std::cout << "It is an operator filter searching for " << x << "in storage, I am still not template, sorry" << std::endl;
